e5edeaba7c5913b68497095b0c5a0b399cf733d8
Fiersik <it.fiersik@gmail.com>
2023-10-16 09:32:45 +0400

    [0.6.1] admin_rule updated

--- src/modules/custom_rules.py
+++ src/modules/custom_rules.py
@@ -7,10 +7,6 @@ class admin_rule(ABCRule[Message]):
     async def check(self, event: Message) -> bool:
 
         members = await event.ctx_api.messages.get_conversation_members(event.peer_id)
-        admins = []
-
-        for member in members.items:
-            if member.is_admin:
-                admins.append(member.member_id)
+        admins = [member.member_id for member in members.items if member.is_admin]
 
         return event.from_id in admins
